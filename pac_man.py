775b293452ede44d5e527f9112c71ecfadf3ad72
Hyclo <miguel-aroldi@bluewin.ch>
2022-12-23 14:07:18 +0100

    clean up & minor changes on the set up functions

--- pac_man.py
+++ pac_man.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
-from game_starter import setup
+import random
+from start_variables import setup
 
 # Constants
 BLACK = (0, 0, 0)
@@ -9,13 +10,19 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-start_variables = setup()
-
-score = start_variables.get("score")
-
 # Game dimensions
 WIDTH = 640
 HEIGHT = 480
+
+# START VARIABLES START::
+start_variables = setup()
+
+score = start_variables.get("score")
+ghost_speed = start_variables.get("ghost_speed")
+ghost_speedup = start_variables.get("ghost_speedup")
+is_game_over = start_variables.get("is_game_over")
+is_loop_running = start_variables.get("is_loop_running")
+# START VARIABLES END::
 
 # Initialize Pygame
 pygame.init()
@@ -26,20 +33,39 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 # Set up the font
 font = pygame.font.Font(None, 36)
 
-# Set up ghost speed
-ghost_speed = 0.0
-ghost_speedup = 0.0
-
-# Set random variabel
-gameOver = False
-running = True
-
 def exit():
     pygame.quit()
     sys.exit()
+    
+def set_up_ghosts(amount):
+    # Set up the ghosts
+    ghosts = []
+    for i in range(amount):
+        ghost_image = pygame.image.load("ghost.png").convert()
+        ghost_rect = ghost_image.get_rect()
+        ghost_rect.x = 580
+        ghost_rect.y = 420
+        ghosts.append(ghost_rect)
+        
+    return (ghosts, ghost_image)
+        
+def set_up_coins(amount):
+    # Set up the dots
+    dot_image = pygame.image.load("dot.png").convert()
+    dot_image_width = 32
+    dot_image_height = 32
+    dots = []
+    for i in range(amount):
+        dot = dot_image.get_rect()
+        dot.x = random.randrange(0 + dot_image_width, WIDTH - dot_image_width, 1)
+        dot.y = random.randrange(0 + dot_image_height, HEIGHT - dot_image_height, 1)
+        dots.append(dot)
+        
+    return (dots, dot_image)
+    
 
 
-def gameLoop(ghost_speed, score, gameOver, running):
+def gameLoop(ghost_speed, score, is_game_over, is_loop_running):
 
     lose = False
 
@@ -50,24 +76,13 @@ def gameLoop(ghost_speed, score, gameOver, running):
     player_image = pygame.image.load("pacman.png").convert()
     player_rect = player_image.get_rect()
 
-    # Set up the ghosts
-    ghost_image = pygame.image.load("ghost.png").convert()
-    ghost_rect = ghost_image.get_rect()
-    ghost_rect.x = 580
-    ghost_rect.y = 420
-
-    # Set up the dots
-    dot_image = pygame.image.load("dot.png").convert()
-    dots = []
-    for i in range(10):
-        dot = dot_image.get_rect()
-        dot.x = i * 60
-        dot.y = i * 40
-        dots.append(dot)
+    ghosts, ghost_image = set_up_ghosts(1)
+    
+    dots, dot_image= set_up_coins(10)
+   
 
     # Set up the game loop
-    
-    while running == True:
+    while is_loop_running:
         # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -84,57 +99,62 @@ def gameLoop(ghost_speed, score, gameOver, running):
         if keys[pygame.K_DOWN]:
             player_rect.y += 5
 
-        # Move the ghost
         
-        if player_rect.x > ghost_rect.x:
-            ghost_rect.x += ghost_speed
-        if player_rect.x < ghost_rect.x:
-            ghost_rect.x -= ghost_speed
-        if player_rect.y > ghost_rect.y:
-            ghost_rect.y += ghost_speed
-        if player_rect.y < ghost_rect.y:
-            ghost_rect.y -= ghost_speed
-        if ghost_rect.right > WIDTH:
-            ghost_rect.left = 0
-
+        # Move the ghost/s
+        for ghost_rect in ghosts:
+            # TODO replace with ghost AI
+            if player_rect.x > ghost_rect.x:
+                ghost_rect.x += ghost_speed
+            if player_rect.x < ghost_rect.x:
+                ghost_rect.x -= ghost_speed
+            if player_rect.y > ghost_rect.y:
+                ghost_rect.y += ghost_speed
+            if player_rect.y < ghost_rect.y:
+                ghost_rect.y -= ghost_speed
+            if ghost_rect.right > WIDTH:
+                ghost_rect.left = 0
+        
+        
         # Draw the screen
         screen.fill(BLACK)
         screen.blit(player_image, player_rect)
-        screen.blit(ghost_image, ghost_rect)
+        
+        for ghost_rect in ghosts:
+            screen.blit(ghost_image, ghost_rect)
+            
         for dot in dots:
             screen.blit(dot_image, dot)
         pygame.display.flip()
 
-        # Check for collisions
-        if player_rect.colliderect(ghost_rect):
-            lose = True
-            print("You lose!")
-            gameOver = True
-            pygame.quit()
-            sys.exit()
-            # running = False
+        # check if player collided with ghost
+        for ghost_rect in ghosts:
+            if player_rect.colliderect(ghost_rect):
+                lose = True
+                print("You lose!")
+                is_game_over = True
+                pygame.quit()
+                sys.exit()
 
-        # Check for dot collection
+        # check if player collided with coin
         for dot in dots:
             if player_rect.colliderect(dot):
                 dots.remove(dot)
 
-        # Check for win --------------------------------------------------------
+        # Check if player has won
         if not dots:
             if lose == False:
                 print("You win!")
                 score += 1
                 print(score)
                 ghost_speed += ghost_speedup
-                gameLoop(ghost_speed, score, gameOver, running)
-                # running = False
-
-        
+                gameLoop(ghost_speed, score, is_game_over, is_loop_running)
 
         # Limit the frame rate
         clock.tick(60)
-if gameOver == False:
-    print("game has been restarted with the start function")
-    gameLoop(ghost_speed, score, gameOver, running)
-# Shut down Pygame
+        
+if is_game_over == False:
+    # game has been restarted with the start function
+    gameLoop(ghost_speed, score, is_game_over, is_loop_running)
+
+# Pygame closes game
 pygame.quit()
